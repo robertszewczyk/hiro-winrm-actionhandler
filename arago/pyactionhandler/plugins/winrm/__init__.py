09f0c33337af6308f7ced397fe5a456845ba0884
Marcus Klemm <mklemm@arago.de>
2017-11-15 11:58:59 +0100

    Fix style errors (flake8)

--- arago/pyactionhandler/plugins/winrm/__init__.py
+++ arago/pyactionhandler/plugins/winrm/__init__.py
@@ -41,58 +41,70 @@ class WinRMCmdAction(Action):
 		return result.group('minor_desc') if result else None
 
 	def winrm_run_script(self, winrm_session):
-		script=self.init_script(self.parameters['Command'])
+		script = self.init_script(self.parameters['Command'])
 		for i in range(3):
 			try:
 				script.run(winrm_session)
 				self.output, self.error_output = script.get_outputs()
 				self.system_rc = script.rs.status_code
-				self.success=True
+				self.success = True
 			except KerberosExchangeError as e:
 				parsed_err = self.parse_krb5_err(str(e))
-				self.statusmsg=str(e)
-				self.success=False
-				self.system_rc=-1
-				if parsed_err == "Ticket expired" or (parsed_err.startswith("Credentials cache file ") and parsed_err.endswith(" not found")):
-					self.logger.warning("[{anum}] No Kerberos ticket for {host}, requesting one ...".format(anum=self.num, host=self.parameters['Hostname']))
-					self.statusmsg="Authentication failed."
+				self.statusmsg = str(e)
+				self.success = False
+				self.system_rc = -1
+				if (parsed_err == "Ticket expired"
+					or (parsed_err.startswith("Credentials cache file ") and parsed_err.endswith(" not found"))):
+					self.logger.warning("[{anum}] No Kerberos ticket for {host}, requesting one ...".format(
+						anum=self.num, host=self.parameters['Hostname']))
+					self.statusmsg = "Authentication failed."
 					try:
 						with open(self.parameters['Keytab']):
 							pass
-						gevent.subprocess.check_output(["kinit", "-k", "-t", self.parameters['Keytab'], self.parameters['Username']], timeout=30, stderr=gevent.subprocess.STDOUT)
-						self.logger.info("[{anum}] Successfully retrieved Kerberos ticket for {host}, retrying command execution.".format(anum=self.num, host=self.parameters['Hostname']))
+						gevent.subprocess.check_output(
+							["kinit", "-k", "-t", self.parameters['Keytab'], self.parameters['Username']],
+							timeout=30, stderr=gevent.subprocess.STDOUT)
+						self.logger.info("[{anum}] Successfully retrieved Kerberos ticket for {host}, retrying command execution.".format(
+							anum=self.num, host=self.parameters['Hostname']))
 					except gevent.subprocess.CalledProcessError as e:
-						self.logger.error("[{anum}] Retrieving Kerberos ticket for {host} failed: {err}".format(anum=self.num, host=self.parameters['Hostname'], err=e.output.decode("utf-8")))
+						self.logger.error("[{anum}] Retrieving Kerberos ticket for {host} failed: {err}".format(
+							anum=self.num, host=self.parameters['Hostname'], err=e.output.decode("utf-8")))
 					except gevent.subprocess.TimeoutExpired as e:
-						self.logger.error("[{anum}] Retrieving Kerberos ticket for {host} timed out!".format(anum=self.num, host=self.parameters['Hostname']))
+						self.logger.error("[{anum}] Retrieving Kerberos ticket for {host} timed out!".format(
+							anum=self.num, host=self.parameters['Hostname']))
 					except FileNotFoundError as e:
-						self.logger.error("[{anum}] Retrieving Kerberos ticket for {host} failed: {err}".format(anum=self.num, host=self.parameters['Hostname'], err=e))
+						self.logger.error("[{anum}] Retrieving Kerberos ticket for {host} failed: {err}".format(
+							anum=self.num, host=self.parameters['Hostname'], err=e))
 					except KeyError:
-						self.logger.error("[{anum}] Retrieving Kerberos ticket for {host} failed! Credentials missing!".format(anum=self.num, host=self.parameters['Hostname']))
+						self.logger.error("[{anum}] Retrieving Kerberos ticket for {host} failed! Credentials missing!".format(
+							anum=self.num, host=self.parameters['Hostname']))
 					continue
 				elif parsed_err:
-					self.logger.error("[{anum}] An error occured during command execution on {node}: Kerberos: {err}".format(anum=self.num, node=self.node,err=parsed_err))
-					self.statusmsg=parsed_err
+					self.logger.error("[{anum}] An error occured during command execution on {node}: Kerberos: {err}".format(
+						anum=self.num, node=self.node, err=parsed_err))
+					self.statusmsg = parsed_err
 				else:
-					self.logger.error("[{anum}] An error occured during command execution on {node}: Kerberos: {err}".format(anum=self.num, node=self.node,err=str(e)))
+					self.logger.error("[{anum}] An error occured during command execution on {node}: Kerberos: {err}".format(
+						anum=self.num, node=self.node, err=str(e)))
 			except (
 					winrm.exceptions.WinRMError,
 					winrm.exceptions.WinRMTransportError,
 					arago.pyactionhandler.plugins.winrm.exceptions.WinRMError,
 					requests.exceptions.RequestException
 			) as e:
-				self.statusmsg=str(e)
-				self.success=False
-				self.system_rc=-1
-				self.logger.error("[{anum}] An error occured during command execution on {node}: {err}".format(anum=self.num, node=self.node,err=str(e)))
+				self.statusmsg = str(e)
+				self.success = False
+				self.system_rc = -1
+				self.logger.error("[{anum}] An error occured during command execution on {node}: {err}".format(
+					anum=self.num, node=self.node, err=str(e)))
 			break
 
 	def __call__(self):
 		try:
-			winrm_session=self.init_direct_session(
-				host = self.parameters['Hostname'],
-				protocol = 'https' if self.parameters['UseSSL'] == 'true' else 'http',
-				port = '5986' if self.parameters['UseSSL'] == 'true' else '5985')
+			winrm_session = self.init_direct_session(
+				host=self.parameters['Hostname'],
+				protocol='https' if self.parameters['UseSSL'] == 'true' else 'http',
+				port='5986' if self.parameters['UseSSL'] == 'true' else '5985')
 		except KeyError:
 			self.logger.error("[{anum}] Parameter 'UseSSL' is missing, check your CapabilityYAML file")
 		self.logger.debug("[{anum}] Connecting directly to '{target}'".format(
@@ -100,8 +112,9 @@ class WinRMCmdAction(Action):
 			target=self.parameters['Hostname']))
 		self.winrm_run_script(winrm_session)
 
+
 class WinRMPowershellAction(WinRMCmdAction):
-	def init_script(self,script):
+	def init_script(self, script):
 		return Script(
 			script=script,
 			interpreter='ps',
