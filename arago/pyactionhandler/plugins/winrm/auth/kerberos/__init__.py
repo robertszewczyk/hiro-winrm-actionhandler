3f8f13aa733296c4eb185172aed435eb8d793c73
Marcus Klemm <mklemm@arago.de>
2017-12-22 11:40:46 +0100

    add logger to krb5Session

--- arago/pyactionhandler/plugins/winrm/auth/kerberos/__init__.py
+++ arago/pyactionhandler/plugins/winrm/auth/kerberos/__init__.py
@@ -1,13 +1,13 @@
 from arago.pyactionhandler.plugins.winrm.session import Session
-import winrm
+import winrm, logging
 
 
 class krb5Session(Session):
-	def __init__(self, endpoint, target=None, validation='ignore', **kwargs):
+	def __init__(self, endpoint, target=None, *args, **kwargs):
 		self.protocol = winrm.Protocol(
 			endpoint=endpoint,
 			transport='kerberos',
-			server_cert_validation=validation,
 			kerberos_delegation=True,
 			**kwargs)
 		self.target = target
+		self.logger = logging.getLogger('root')
