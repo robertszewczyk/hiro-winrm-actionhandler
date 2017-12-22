bb3c2adb05f98a4ed861b3b4563bb5abfa904a60
Marcus Klemm <mklemm@arago.de>
2017-12-22 13:25:57 +0100

    Enable jumpserver mode again

--- arago/pyactionhandler/plugins/winrm/session.py
+++ arago/pyactionhandler/plugins/winrm/session.py
@@ -11,19 +11,28 @@ class Session(winrm.Session):
 		self.logger = logging.getLogger('root')
 		return super().__init__(*args, **kwargs)
 
-	def run_ps(self, script):
+	def run_ps(self, script, target=None, use_ssl=False, creds=None):
 		"""base64 encodes a Powershell script and executes the powershell encoded script command"""
 
-		# must use utf16 little endian on windows
 		script_bytes = script.encode('utf-8')
 		packed_expression = base64.b64encode(script_bytes).decode('cp850')
 		expression_template = (
- 			'Invoke-Expression '
- 			'$([System.Text.Encoding]::UTF8.GetString('
- 				"[Convert]::FromBase64String('{0}'))"  # NOQA
- 				')'
- 			)
-		command = expression_template.format(packed_expression)
+			'iex $(' +
+			"[System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('{0}'))".format(packed_expression)
+			+ ')')
+		if target:
+			if creds:
+				username, password = creds
+			expression_template = "{gencreds}icm {cmpname}{creds}{usessl}-Command {{{expr}}}".format(
+				gencreds=("$u='{usr}'; $p=ConvertTo-SecureString '{passwd}' -AsPlainText "
+						  '-Force; $c = new-object -Typename System.Management.Automation.PSCredential '
+						  '-Args $u,$p;').format(usr=username, passwd=password) if creds else "",
+				cmpname="-Cn {target} ".format(target=target) if target else "",
+				creds="-Credential $c " if creds else "",
+				usessl="-UseSSL " if use_ssl else "",
+				expr=expression_template
+			)
+		command = '$OutputEncoding=[console]::OutputEncoding=[console]::InputEncoding=[system.text.encoding]::GetEncoding([System.Text.Encoding]::Default.CodePage);' + expression_template
 		exe = 'mode con: cols=1052 & powershell -NoProfile -command "{0}"'.format(command)
 		rs = self.run_cmd(exe)
 		if rs.std_err:
